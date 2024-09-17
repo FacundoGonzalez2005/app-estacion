73b9cfdf78cc7217a6a3061f7e90ab89e03db7db
FacundoGonzalez2005 <facundo.gonza2005@gmail.com>
2024-09-16 21:25:53 -0300

    Se borro las variables y funciones que no servian para cargar la vista landing

--- controllers/landingController.php
+++ controllers/landingController.php
@@ -3,12 +3,6 @@
 	// carga la vista
 	$tpl = new Motor("landing");
 
-	// variables a reemplazar en la plantilla con sus valores
-	$var = ["CANT_LUGARES" => 4];
-
-	// reemplaza las variables de la vista con los valores del vector
-	$tpl->setVars($var);
-
 	// imprime la vista en la página
 	$tpl->print();
 
